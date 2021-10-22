372ea81c53958767c4f68894a40f63891bfd89d5
Tornac <martin.volkmann96@googlemail.com>
2021-10-23 00:31:11 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './style.css'
 import { uniqueId } from 'lodash'
 import ShapeSvg from './ShapeSvg'
 import CirclesTable from './CirclesTable.js'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App() {
     const [width, setWidth] = React.useState(200)
@@ -17,10 +19,26 @@ export default function App() {
             newShapes.push(oldShape.id === newShape.id ? newShape : oldShape)
         setShapes(newShapes)
     }
+
+    function share() {
+        const code = JSON.stringify(
+            shapes.map(it => {
+                return { ...it, id: undefined }
+            })
+        )
+        window.navigator.clipboard.writeText(code).then(() => toast('copied code to clipboard'))
+    }
+
+    function load() {
+        window.navigator.clipboard.readText().then(text => console.log(text))
+    }
     return (
         <div>
             <ShapeSvg width={width} height={height} shapes={shapes} updateShape={updateShape} />
-            <CirclesTable circles={shapes.filter(it => it.type === 'circle')} />
+            {/*<CirclesTable circles={shapes.filter(it => it.type === 'circle')} />*/}
+            <button onClick={share}>Share</button>
+            <button onClick={load}>Load</button>
+            <ToastContainer />
         </div>
     )
 }
